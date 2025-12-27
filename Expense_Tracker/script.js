044ea22dcadaab47227f151029f42efafe97d2a6
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-12-27 14:10:11 +0530

    added some media queries for responsive desgin for the mobile devices

--- Expense_Tracker/script.js
+++ Expense_Tracker/script.js
@@ -1,27 +1,27 @@
-const balance_element = document.getElementById('balance');
-const incomeAmtelement = document.getElementById('income-amount');
-const expensesAmtelement = document.getElementById('expenses-amount');
-const transactionListelements = document.getElementById('transaction-list');
-const transactionFormelement = document.getElementById('transaction-form');
-const descriptionElement = document.getElementById('description');
+const balance_element = document.getElementById("balance");
+const incomeAmtelement = document.getElementById("income-amount");
+const expensesAmtelement = document.getElementById("expenses-amount");
+const transactionListelements = document.getElementById("transaction-list");
+const transactionFormelement = document.getElementById("transaction-form");
+const descriptionElement = document.getElementById("description");
 const amountElement = document.getElementById("amount");
 
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-transactionFormelement.addEventListener('submit',addTransaction);
+transactionFormelement.addEventListener("submit", addTransaction);
 
-function addTransaction(e){
+function addTransaction(e) {
   e.preventDefault();
-  
+
   const description = descriptionElement.value.trim();
   const amount = parseFloat(amountElement.value);
 
   transactions.push({
-    id:Date.now(),
+    id: Date.now(),
     description,
-    amount
-  })
-  localStorage.setItem("transactions",JSON.stringify(transactions))
+    amount,
+  });
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 
   updateTransactionList();
   updateSummary();
@@ -29,33 +29,40 @@ function addTransaction(e){
   transactionFormelement.reset();
 }
 
-function updateTransactionList(){
+function updateTransactionList() {
   transactionListelements.innerHTML = "";
   const sortedList = [...transactions].reverse();
 
-  sortedList.forEach((transaction)=>{
+  sortedList.forEach((transaction) => {
     const transactionele = createTransactionEle(transaction);
     transactionListelements.appendChild(transactionele);
-  })
+  });
 }
 
-function createTransactionEle(transaction){
-  const li = document.createElement('li');
+function createTransactionEle(transaction) {
+  const li = document.createElement("li");
   li.classList.add("transaction");
-  li.classList.add(transaction.amount >0 ? "income":"expenses");
+  li.classList.add(transaction.amount > 0 ? "income" : "expenses");
   li.innerHTML = `
     <span>${transaction.description}</span>
     <span>${formatCurrency(transaction.amount)}
-      <button class="delete-btn" onclick=removeTransaction(${transaction.id})>x</button>
+      <button class="delete-btn" onclick=removeTransaction(${
+        transaction.id
+      })>x</button>
     </span>
-  `
+  `;
   return li;
 }
 
-function updateSummary(){
-  const balance = transactions.reduce((acc,transaction)=>acc+transaction.amount,0);
+function updateSummary() {
+  const balance = transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
 
-  const income = transactions.filter(transaction => transaction.amount>0).reduce((acc,transaction)=>acc+transaction.amount,0);
+  const income = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
 
   const expenses = balance - income;
 
@@ -67,14 +74,14 @@ function updateSummary(){
 function formatCurrency(number) {
   return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "INR"
+    currency: "INR",
   }).format(number);
 }
 
-function removeTransaction(id){
-  transactions = transactions.filter(transaction => transaction.id !== id)
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
 
-  localStorage.setItem("transactions",JSON.stringify(transactions));
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 
   updateTransactionList();
   updateSummary();
