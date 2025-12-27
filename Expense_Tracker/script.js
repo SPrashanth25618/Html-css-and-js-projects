bbd526903acebfca1dd44e3ec0206a4489355f15
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-12-27 12:17:00 +0530

    Done with getting the dom elements in the js file

---
+++ Expense_Tracker/script.js
@@ -0,0 +1,8 @@
+const balance_element = document.getElementById('balance');
+const incomeAmtelement = document.getElementById('income-amount');
+const expensesAmtelement = document.getElementById('expenses-amount');
+const transactionListelements = document.getElementById('transaction-list');
+const transactionFormelement = document.getElementById('transaction-form');
+const descriptionElement = document.getElementById('description');
+const amountElement = document.getElementById("amount");
+
