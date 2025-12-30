94986aeb07b2c1b7fbff67d15fef64682af72f54
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-12-30 15:10:04 +0530

    Completed the project

--- Form_Validator/script.js
+++ Form_Validator/script.js
@@ -12,7 +12,60 @@ form.addEventListener("submit", function (e) {
     password,
     confirmPassword,
   ]);
+  
+  let isFormValid = requiredisFilled;
+  if(requiredisFilled){
+    const isUsernameValid = checklength(username,3,15);    
+    const isEmailValid = checkEmail(email);
+    const isPasswordValid = checklength(password,6,25);
+    const isPasswordsMatch = checkPasswordsMatch(password,confirmPassword);
+    isFormValid = isUsernameValid && isEmailValid && isPasswordValid && isPasswordsMatch;
+  }
+  if(isFormValid){
+    alert("Registration Successfull");
+    form.reset();
+    document.querySelectorAll('.form-group').forEach(group => {
+      group.className = "form-group";
+    })
+  }
 });
+
+function checkPasswordsMatch(password,confirmPassword){
+  if(password.value !== confirmPassword.value){
+    showError(password,"Passwords are not matched");
+    showError(confirmPassword,"Passwords are not matched");
+    return false;
+  }
+  else{
+    showSuccess(password);
+    showError(confirmPassword);
+    return true;
+  }
+}
+
+function checkEmail(email){
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(emailRegex.test(email.value.trim())){
+    showSuccess(email);
+    return true;
+  }else{
+    showError(email,"Email is not valid");
+    return false;
+  }
+}
+
+function checklength(input,min,max){
+  if(input.value.length < min){
+    showError(input,`${formatFieldName(input)} must be at least ${min} characters.`)
+    return false;
+  }else if(input.value.length > max){
+    showError(input,`${formatFieldName(input)} must be less than ${max} characters.`);
+    return false;
+  }else{
+    showSuccess(input);
+    return true;
+  }  
+}
 
 function checkRequired(arr) {
   let isvalid = true;
@@ -40,6 +93,5 @@ function showError(input,message){
 
 function showSuccess(input){
   const formGroup = input.parentElement;
-  formGroup.className = "form-group success";
-
+  formGroup.className = "form-group success";  
 }
